ed8d51522a7a17df509794b35461cd6ba529fc5a
DearKira <45229542+DearKira@users.noreply.github.com>
2019-02-11 14:32:06 +0100

    Nouvel interface ajoutée

--- src/main/java/Panel.java
+++ src/main/java/Panel.java
@@ -16,14 +16,17 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.concurrent.TimeUnit;
 import java.awt.event.ActionEvent;
 import javax.swing.JLayeredPane;
 import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
 import java.awt.SystemColor;
+import java.awt.MenuBar;
 
 public class Panel extends JFrame {
 	private JTextField pseudo, password;
+	
 	private Label connStatut;
 	private JPanel Login_Panel, Menu;
 	public Panel() throws SQLException {
@@ -45,6 +48,7 @@ public class Panel extends JFrame {
 		Login_Panel.setLayout(null);
 		
 		JPanel entete = new JPanel();
+		
 		entete.setBackground(Color.ORANGE);
 		entete.setBounds(10, 11, 424, 118);
 		Login_Panel.add(entete);
@@ -73,12 +77,15 @@ public class Panel extends JFrame {
 					if(base.CheckConnexion(pseudo.getText(), password.getText())) {
 						connStatut.setForeground(Color.GREEN);
 						connStatut.setText("Connexion en cours..");
+						TimeUnit.SECONDS.sleep(2);
+						layeredPane.setLayer(Login_Panel, 0);
+						layeredPane.setLayer(Menu, 1);
 					}
 					else {
 						connStatut.setForeground(Color.RED);
 						connStatut.setText("Identifiants incorrects !");
 					}						
-				} catch (SQLException e) {
+				} catch (SQLException | InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
@@ -86,6 +93,11 @@ public class Panel extends JFrame {
 		});
 		btnConnexion.setBounds(231, 73, 89, 23);
 		corps.add(btnConnexion);
+		
+		connStatut = new Label("Veuillez vous identifier.");
+		connStatut.setAlignment(Label.CENTER);
+		connStatut.setBounds(231, 104, 193, 14);
+		corps.add(connStatut);
 		
 		JButton btnQuitter = new JButton("Quitter");
 		btnQuitter.addActionListener(new ActionListener() {
@@ -110,16 +122,55 @@ public class Panel extends JFrame {
 		label.setBounds(0, 107, 215, 22);
 		corps.add(label);
 		
-		connStatut = new Label("Veuillez vous identifier.");
-		connStatut.setAlignment(Label.CENTER);
-		connStatut.setBounds(231, 104, 193, 14);
-		corps.add(connStatut);
-		
 		Menu = new JPanel();
 		layeredPane.setLayer(Menu, 0);
 		Menu.setBounds(0, 0, 444, 271);
 		layeredPane.add(Menu);
 		Menu.setLayout(null);
+		
+		JPanel entete_menu = new JPanel();
+		entete_menu.setLayout(null);
+		entete_menu.setBackground(Color.GREEN);
+		entete_menu.setBounds(10, 11, 424, 118);
+		Menu.add(entete_menu);
+		
+		JPanel corps_menu = new JPanel();
+		corps_menu.setBounds(10, 131, 424, 129);
+		Menu.add(corps_menu);
+		corps_menu.setLayout(null);
+		
+		Label label_3 = new Label("2018 - Maison des Ligues inc.");
+		label_3.setFont(new Font("DejaVu Sans Condensed", Font.ITALIC, 12));
+		label_3.setEnabled(false);
+		label_3.setBounds(0, 107, 215, 22);
+		corps_menu.add(label_3);
+		
+		Label labelConnected = new Label("Connecté en tant que Admin");
+		labelConnected.setBounds(10, 11, 137, 14);
+		corps_menu.add(labelConnected);
+		
+		JButton btnDeconnexion = new JButton("D\u00E9connexion");
+		btnDeconnexion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					base.CloseConnexion();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				password.setText("");
+				connStatut.setForeground(Color.BLACK);
+				connStatut.setText("Veuillez vous identifier.");
+				layeredPane.setLayer(Login_Panel, 1);
+				layeredPane.setLayer(Menu, 0);
+			}
+		});
+		btnDeconnexion.setBounds(10, 36, 137, 23);
+		corps_menu.add(btnDeconnexion);
+		
+		JButton btnDeclarePanne = new JButton("D\u00E9clarer une panne");
+		btnDeclarePanne.setBounds(277, 7, 137, 23);
+		corps_menu.add(btnDeclarePanne);
 		
 	}
 }
